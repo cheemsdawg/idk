8a6193d7ba70f35cc4aab91e991f552355dc59fd
cheemsdawg <87579411+cheemsdawg@users.noreply.github.com>
2022-12-27 23:07:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import cvlib as cv
 
-faces, confidences = cv.detect_face("idk.jpeg")
+faces, confidences = cv.detect_face("AEBC9D04-6561-4AD9-988F-55DA62408547.jpeg")
 
 label, confidence = cv.detect_gender(face)
 
